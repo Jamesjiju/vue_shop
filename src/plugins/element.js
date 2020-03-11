f0300d5516c2cadfa7a9e41c8c0a7b08405a4b99
herui01 <1094310681@qq.com>
2020-03-11 22:13:43 +0800

    newFile

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 需要将message挂载到vue上面才可以生效,$message是自定义的，可以叫别的名称
+Vue.prototype.$message = Message
