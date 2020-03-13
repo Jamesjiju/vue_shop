2635ba6d4b06af2a4bee3513c6fcf973c5764a4c
herui01 <1094310681@qq.com>
2020-03-13 22:32:01 +0800

    vuell

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,7 +17,7 @@
                 </el-form-item>
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
-                    <el-button type="primary" @click="login">登录11</el-button>
+                    <el-button type="primary" @click="login">登录112</el-button>
                     <el-button type="info" @click="resetLoginForm">重置1</el-button>
                 </el-form-item>
             </el-form>
